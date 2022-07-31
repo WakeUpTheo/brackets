a6603b3ebe33c7d3faeee2df3a23c613fe1c348b
Fedor Parenkov <parenkov.fedor@gmail.com>
2022-07-31 13:54:39 +0400

    feat: add function for checking valid brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const openBrackets = [];
+  const pairs = {};
+
+  bracketsConfig.forEach(element => {
+      openBrackets.push(element[0]);
+      pairs[element[1]] = element[0];
+  });
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+      let current = str[i];
+
+      if (stack.length !== 0) {
+          let topElement = stack[stack.length - 1];
+          if (pairs[current] === topElement) {
+              stack.pop();
+          } else {
+              stack.push(current);
+          }
+      } else {
+          if (openBrackets.includes(current)) {
+              stack.push(current);
+          } else {
+              return false;
+          }
+      }
+  }
+  return stack.length === 0;
 }
